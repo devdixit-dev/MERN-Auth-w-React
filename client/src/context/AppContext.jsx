26443ee18d9628c2b7035d2f6acbddc799bb0195
Devarsh Dixit ( Dev ) <devdixitsocial@gmail.com>
2025-02-23 15:58:54 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContextProvider = (props) => {
 
   axios.defaults.withCredentials = true
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const backendUrl = "https://auth-app-frk1.onrender.com"
 
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(false);
